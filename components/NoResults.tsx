9a8231fddb4b80972fac72fccfe61c7f27bd83a2
Regtables <reghardt7@gmail.com>
2022-09-08 15:57:30 +0200

    types init, sidebar, sanity conntection, api route

---
+++ components/NoResults.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface iProps {
+  text: string
+}
+
+const NoResults = ({ text }: iProps) => {
+  return (
+    <div>NoResults</div>
+  )
+}
+
+export default NoResults
---
+++ types.d.ts
@@ -0,0 +1,32 @@
+export interface Video {
+  caption: string,
+  video: {
+    asset: {
+      _id: string,
+      url: string
+    }
+  },
+  _id: string,
+  userId: string,
+  postedBy: {
+    _id: string,
+    userName: string,
+    image: string
+  }
+  comments: {
+    comment: string,
+    _key: string
+    postedBy: {
+      _id: string,
+      userName: string,
+      image: string
+    }
+  }[],
+  likes: {
+    postedBy: {
+      _id: string,
+      userName: string,
+      image: string
+    }
+  }[],
+}
